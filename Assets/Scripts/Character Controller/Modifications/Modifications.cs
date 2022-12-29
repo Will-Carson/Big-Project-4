7b167b009274845515c235bd53ef36694f581f32
William Carson <williamacarson1@outlook.com>
2022-12-28 22:52:54 -0500

    Removed some more of "my" stuff.

--- Assets/Scripts/Character Controller/Modifications/Modifications.cs
+++ Assets/Scripts/Character Controller/Modifications/Modifications.cs
@@ -23,9 +23,6 @@ public struct PlatformerCharacterStateMachine : IComponentData
     public LedgeStandingUpState LedgeStandingUpState;
     public FlyingNoCollisionsState FlyingNoCollisionsState;
     public RopeSwingState RopeSwingState;
-    public FiringState CastingState;
-    public StunnedState StunnedState;
-    public DeadState DeadState;
 
     public void TransitionToState(CharacterState newState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
     {
@@ -76,15 +73,6 @@ public struct PlatformerCharacterStateMachine : IComponentData
             case CharacterState.RopeSwing:
                 RopeSwingState.OnStateEnter(previousState, ref context, ref baseContext, in aspect);
                 break;
-            case CharacterState.Casting:
-                CastingState.OnStateEnter(previousState, ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Stunned:
-                StunnedState.OnStateEnter(previousState, ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Dead:
-                DeadState.OnStateEnter(previousState, ref context, ref baseContext, in aspect);
-                break;
         }
     }
 
@@ -127,15 +115,6 @@ public struct PlatformerCharacterStateMachine : IComponentData
                 break;
             case CharacterState.RopeSwing:
                 RopeSwingState.OnStateExit(newState, ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Casting:
-                CastingState.OnStateExit(newState, ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Stunned:
-                StunnedState.OnStateExit(newState, ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Dead:
-                DeadState.OnStateExit(newState, ref context, ref baseContext, in aspect);
                 break;
         }
     }
@@ -180,15 +159,6 @@ public struct PlatformerCharacterStateMachine : IComponentData
             case CharacterState.RopeSwing:
                 RopeSwingState.OnStatePhysicsUpdate(ref context, ref baseContext, in aspect);
                 break;
-            case CharacterState.Casting:
-                CastingState.OnStatePhysicsUpdate(ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Stunned:
-                StunnedState.OnStatePhysicsUpdate(ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Dead:
-                DeadState.OnStatePhysicsUpdate(ref context, ref baseContext, in aspect);
-                break;
         }
     }
 
@@ -231,15 +201,6 @@ public struct PlatformerCharacterStateMachine : IComponentData
                 break;
             case CharacterState.RopeSwing:
                 RopeSwingState.OnStateVariableUpdate(ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Casting:
-                CastingState.OnStateVariableUpdate(ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Stunned:
-                StunnedState.OnStateVariableUpdate(ref context, ref baseContext, in aspect);
-                break;
-            case CharacterState.Dead:
-                DeadState.OnStateVariableUpdate(ref context, ref baseContext, in aspect);
                 break;
         }
     }
@@ -287,15 +248,6 @@ public struct PlatformerCharacterStateMachine : IComponentData
             case CharacterState.RopeSwing:
                 RopeSwingState.GetCameraParameters(in character, out cameraTarget, out calculateUpFromGravity);
                 break;
-            case CharacterState.Casting:
-                CastingState.GetCameraParameters(in character, out cameraTarget, out calculateUpFromGravity);
-                break;
-            case CharacterState.Stunned:
-                StunnedState.GetCameraParameters(in character, out cameraTarget, out calculateUpFromGravity);
-                break;
-            case CharacterState.Dead:
-                DeadState.GetCameraParameters(in character, out cameraTarget, out calculateUpFromGravity);
-                break;
         }
     }
 
@@ -341,198 +293,7 @@ public struct PlatformerCharacterStateMachine : IComponentData
             case CharacterState.RopeSwing:
                 RopeSwingState.GetMoveVectorFromPlayerInput(in inputs, cameraRotation, out moveVector);
                 break;
-            case CharacterState.Casting:
-                RopeSwingState.GetMoveVectorFromPlayerInput(in inputs, cameraRotation, out moveVector);
-                break;
-            case CharacterState.Stunned:
-                RopeSwingState.GetMoveVectorFromPlayerInput(in inputs, cameraRotation, out moveVector);
-                break;
-            case CharacterState.Dead:
-                RopeSwingState.GetMoveVectorFromPlayerInput(in inputs, cameraRotation, out moveVector);
-                break;
         }
-    }
-}
-
-public struct FiringState : IPlatformerCharacterState
-{
-    Entity weaponEntity;
-    float remainingCastTime;
-
-    public void OnStateEnter(CharacterState previousState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-        ref var character = ref aspect.Character.ValueRW;
-        aspect.SetCapsuleGeometry(character.StandingGeometry.ToCapsuleGeometry());
-    }
-
-    public void OnStateExit(CharacterState nextState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-        // Clean up state data
-        weaponEntity = Entity.Null;
-        remainingCastTime = 0;
-
-        ref var character = ref aspect.Character.ValueRW;
-
-        character.IsOnStickySurface = false;
-        character.IsSprinting = false;
-    }
-
-    public void OnStatePhysicsUpdate(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-        var deltaTime = baseContext.Time.DeltaTime;
-        var elapsedTime = (float)baseContext.Time.ElapsedTime;
-        ref var characterBody = ref aspect.CharacterAspect.CharacterBody.ValueRW;
-        ref var character = ref aspect.Character.ValueRW;
-        ref var characterControl = ref aspect.CharacterControl.ValueRW;
-        ref var characterPosition = ref aspect.CharacterAspect.LocalTransform.ValueRW.Position;
-        ref var characterRotation = ref aspect.CharacterAspect.LocalTransform.ValueRW.Rotation;
-
-        aspect.HandlePhysicsUpdatePhase1(ref context, ref baseContext, true, true);
-
-        remainingCastTime -= deltaTime;
-
-        if (characterBody.IsGrounded)
-        {
-            // Rotate character towards target
-            CharacterControlUtilities.SlerpRotationTowardsDirectionAroundUp(ref characterRotation, deltaTime, math.normalizesafe(characterControl.MoveVector), MathUtilities.GetUpFromRotation(characterRotation), character.AirRotationSharpness);
-
-            // Once the cast time <= 0, cast the ability
-            if (remainingCastTime <= 0)
-            {
-                //var abilityEntity = Entity.Null;
-                //context.EndFrameECB.SetComponent(context.ChunkIndex, abilityEntity, LocalTransform.FromPositionRotation(characterPosition, characterRotation));
-            }
-        }
-
-        aspect.HandlePhysicsUpdatePhase2(ref context, ref baseContext, true, true, true, true, true);
-
-        DetectTransitions(ref context, ref baseContext, in aspect);
-    }
-
-    public void OnStateVariableUpdate(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void GetCameraParameters(in PlatformerCharacterComponent character, out Entity cameraTarget, out bool calculateUpFromGravity)
-    {
-        cameraTarget = character.DefaultCameraTargetEntity;
-        calculateUpFromGravity = !character.IsOnStickySurface;
-    }
-
-    public void GetMoveVectorFromPlayerInput(in PlatformerPlayerInputs inputs, quaternion cameraRotation, out float3 moveVector)
-    {
-        PlatformerCharacterAspect.GetCommonMoveVectorFromPlayerInput(in inputs, cameraRotation, out moveVector);
-    }
-
-    public bool DetectTransitions(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-        ref KinematicCharacterBody characterBody = ref aspect.CharacterAspect.CharacterBody.ValueRW;
-        ref PlatformerCharacterControl characterControl = ref aspect.CharacterControl.ValueRW;
-        ref PlatformerCharacterStateMachine stateMachine = ref aspect.StateMachine.ValueRW;
-
-        if (characterControl.CrouchPressed)
-        {
-            stateMachine.TransitionToState(CharacterState.Crouched, ref context, ref baseContext, in aspect);
-            return true;
-        }
-
-        if (characterControl.RollHeld)
-        {
-            stateMachine.TransitionToState(CharacterState.Rolling, ref context, ref baseContext, in aspect);
-            return true;
-        }
-
-        if (characterControl.DashPressed)
-        {
-            stateMachine.TransitionToState(CharacterState.Dashing, ref context, ref baseContext, in aspect);
-            return true;
-        }
-
-        if (!characterBody.IsGrounded)
-        {
-            stateMachine.TransitionToState(CharacterState.AirMove, ref context, ref baseContext, in aspect);
-            return true;
-        }
-
-        if (characterControl.ClimbPressed)
-        {
-            if (ClimbingState.CanStartClimbing(ref context, ref baseContext, in aspect))
-            {
-                stateMachine.TransitionToState(CharacterState.Climbing, ref context, ref baseContext, in aspect);
-                return true;
-            }
-        }
-
-        return aspect.DetectGlobalTransitions(ref context, ref baseContext);
-    }
-}
-
-public struct StunnedState : IPlatformerCharacterState
-{
-    public void GetCameraParameters(in PlatformerCharacterComponent character, out Entity cameraTarget, out bool calculateUpFromGravity)
-    {
-        cameraTarget = Entity.Null;
-        calculateUpFromGravity = false;
-    }
-
-    public void GetMoveVectorFromPlayerInput(in PlatformerPlayerInputs inputs, quaternion cameraRotation, out float3 moveVector)
-    {
-        moveVector = new float3();
-    }
-
-    public void OnStateEnter(CharacterState previousState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void OnStateExit(CharacterState nextState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void OnStatePhysicsUpdate(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void OnStateVariableUpdate(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-}
-
-public struct DeadState : IPlatformerCharacterState
-{
-    public void GetCameraParameters(in PlatformerCharacterComponent character, out Entity cameraTarget, out bool calculateUpFromGravity)
-    {
-        cameraTarget = Entity.Null;
-        calculateUpFromGravity = false;
-    }
-
-    public void GetMoveVectorFromPlayerInput(in PlatformerPlayerInputs inputs, quaternion cameraRotation, out float3 moveVector)
-    {
-        moveVector = new float3();
-    }
-
-    public void OnStateEnter(CharacterState previousState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void OnStateExit(CharacterState nextState, ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void OnStatePhysicsUpdate(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
-    }
-
-    public void OnStateVariableUpdate(ref PlatformerCharacterUpdateContext context, ref KinematicCharacterUpdateContext baseContext, in PlatformerCharacterAspect aspect)
-    {
-
     }
 }
 
@@ -553,6 +314,4 @@ public enum CharacterState
     FlyingNoCollisions,
     RopeSwing,
     Casting,
-    Stunned,
-    Dead,
 }
